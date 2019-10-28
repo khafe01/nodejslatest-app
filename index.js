a56b3f9d0716aa075cd864fb23faed6b1695643c
Ferdouds Khaleque <khafe01@gmail.com>
2019-10-27 21:42:24 -0400

    Adding new code again latest update 75

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   //  .use(nocache) // Prevents caching of *only* this request
  res.send("hey CSP world, this is very beginning of implementing CICD piplinew latest and new updates 70");
 

e944610af28733245b25f37f25dc9602f09de403
Ferdouds Khaleque <khafe01@gmail.com>
2019-10-27 23:34:10 -0400

    Adding new code again latest update 79

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
-createServer(function(req, res) {
-  res.writeHead(200, {'content-type':'text/plain'});
-  res.end('Hello World!');
+
+createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
 }).listen(3000);
-console.log('Server is running on a port over 3000!!!');

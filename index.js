8c2da940b766b14f7d7be11c132a73188c642ec6
Ferdouds Khaleque <khafe01@gmail.com>
2019-10-27 22:52:48 -0400

    Adding new code again latest update 76

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require("express");
-const app = express();
+import express from 'express';
+const app = express()
 
-app.get("/", function (req, res) {
-  //  .use(nocache) // Prevents caching of *only* this request
- res.send("hey CSP world, this is very beginning of implementing CICD piplinew latest and new updates 70");
+app.get('/', (req, res) => res.send('Hello World!'))
 
+var server = app.listen(3000, () => {
+    console.log("Listening on port " + server.address().port + "...");
 });
-
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
- 
-export default app;

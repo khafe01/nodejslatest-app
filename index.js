1713540d916f9f01cde4390d9291fc7aa151f474
Ferdouds Khaleque <khafe01@gmail.com>
2019-10-27 23:16:38 -0400

    Adding new code again latest update 78

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import express from "express";
-const app = express()
-
-app.get("/", (req, res) => res.send("Hello World!"))
-
-var server = app.listen(3000, () => {
-    console.log("Listening on port " + server.address().port + "...");
-});
+import { createServer } from 'http';
+createServer(function(req, res) {
+  res.writeHead(200, {'content-type':'text/plain'});
+  res.end('Hello World!');
+}).listen(3000);
+console.log('Server is running on a port over 3000!!!');

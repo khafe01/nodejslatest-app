1a86996c991c0f6a395a2a70247d73275dcb53c3
Ferdouds Khaleque <khafe01@gmail.com>
2019-10-27 23:55:13 -0400

    Adding new code again latest update 82

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require("express");
+import express from "express";
 var app = express();
 
 app.get("/", function (req, res) {
   //  .use(nocache) // Prevents caching of *only* this request
- res.send("hey CSP world, this is very beginning of implementing CICD piplinew latest and new updates 66");
+ res.send("hey CSP world, this is very beginning of implementing CICD piplinew latest and new updates 75");
 
 });
 
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
-module.exports = app;
+export default app;

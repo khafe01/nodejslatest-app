9363eba5b1fac0846afcf78b2d8f58459d1c26fd
Ferdouds Khaleque <khafe01@gmail.com>
2019-11-05 11:34:09 -0500

    Adding new code again latest update 13

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.get('/', function (req, res) {
   //  .use(nocache) // Prevents caching of *only* this request
- res.send('Hey world, this is very beginning of implementing CICD pipeline - VERSION 5');
+ res.send('Hey world, this is very beginning of implementing CICD pipeline - VERSION 6');
 
 });
 
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3000);
  
 module.exports = app;

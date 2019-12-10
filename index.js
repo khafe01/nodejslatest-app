f2fe5d6ae3113ff58c9e491affa215b5f5d7fb6f
Ferdouds Khaleque <khafe01@gmail.com>
2019-12-10 09:38:20 -0500

    Adding new code again latest update 26

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   //  .use(nocache) // Prevents caching of *only* this request
- res.send('Hey world, this is very beginning of implementing CICD pipeline - VERSION 5');
+ res.send('Hey world, this is very beginning of implementing CICD pipeline - VERSION 6');
 
 });
 
